1f8352a61220a50c4bb6f8e01d7fc59f19b62e30
Luis Felipe <202makertv@gmail.com>
2022-04-01 11:46:45 -0300

    Pequenas alterações no Header

--- components/Header/BaseComponent.vue
+++ components/Header/BaseComponent.vue
@@ -1,34 +1,36 @@
 <template>
   <header>
     <div class="overlay"></div>
-    <div class="header__content">
-      <h1>Blog</h1>
-    </div>
+    <HeaderContentComponent />
   </header>
 </template>
 
 <script lang=ts>
-
+export default {
+  name: 'ContentComponent'
+}
 </script>
 
 <style scoped>
 header {
   background-image: url("../../static/images/bg.jpg");
   width: 100%;
-  min-height: 50vh;
+  min-height: 70vh;
   position: relative;
   top: 0;
   left: 0;
+  color: white;
+  z-index: 0;
+  background-position: bottom;
+  background-size: cover;
+  background-attachment: fixed;
 }
 
 .overlay {
   background-color: rgba(0, 0, 0, 0.5);
   width: 100%;
-  min-height: 50vh;
+  min-height: 70vh;
   position: absolute;
-}
-
-.header__content {
-  text-align: center;
+  z-index: 1;
 }
 </style>
---
+++ components/Header/ContentComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header__content">
+    <h1>
+      <nuxtLink to="/">Blog</nuxtLink>
+    </h1>
+  </div>
+</template>
+
+<script lang=ts>
+export default {
+  name: 'ContentComponent'
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: white;
+}
+
+h1 {
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  font-size: 48px;
+}
+
+.header__content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+}
+</style>
